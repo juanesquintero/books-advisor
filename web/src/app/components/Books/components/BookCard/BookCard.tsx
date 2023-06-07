6b99242260f325e9dc0b11cc71c0e8ad2259ba34
juan_quintero <juan_quintero@epam.com>
2023-06-06 23:15:30 -0500

    feat: books list page

---
+++ web/src/app/components/Books/components/BookCard/BookCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './BookCard.scss';
+import Button from '@src/app/common/Button/Button';
+import Tag from '@src/app/common/Tag/Tag';
+import { IBookCardProps } from './BookCard.types';
+
+const BookCard: React.FC<IBookCardProps> = (book: IBookCardProps) => {
+	const { title, description, contributor, author, price, publisher } = book;
+	console.log(book);
+	return (
+		<section className='book-card'>
+			<div>
+				<h5>{title}</h5>
+				<p>{description}</p>
+				<Tag label='Contributor' value={contributor} />
+				<Tag label='Author' value={author} />
+			</div>
+
+			<div className='book-card-column'>
+				<Tag label='Price' value={price} />
+				<Tag label='Publisher' value={publisher} />
+				<Button
+					text='Detail'
+					style='outline-dark'
+					icon='eye'
+					size='sm'
+					className='mt-5'
+				/>
+			</div>
+		</section>
+	);
+};
+
+export default BookCard;
---
+++ web/src/app/components/Books/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,17 @@
+import Button from '@src/app/common/Button/Button';
+import Input from '@src/app/common/Input/Input';
+import React from 'react';
+import './SearchBar.scss';
+
+const SearchBar: React.FC = () => {
+	return (
+		<section className='search-bar'>
+			<span className='search-bar-column'>
+				<Input className='search-bar__input' type='text' size='sm' />
+				<Button text='Search' style='light' size='sm' className='m-3' />
+			</span>
+		</section>
+	);
+};
+
+export default SearchBar;
